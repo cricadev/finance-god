5aae9212acbf732b59bedc04c6181e674d90cff0
Cristian David Casallas Velandia <cricadev@Cristians-MacBook-Pro.local>
2022-06-19 22:08:53 -0500

    implementing navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,30 @@
 <template lang="en">
-  <div>
-    this is the {{this.$route.name}}
+<!-- USER CARD -->
+  <div class="flex items-center justify-start w-full my-8 ml-8 font-bold user-card">
+    <img src="../../images/avatar.png" alt="" class="w-10 h-10 ">
+    <div class="">
+      <p class="ml-4 text-xl ">Welcome Back</p>
+      <p class="ml-4 text-sm font-semibold ">Kristin Watson</p>
+    </div>
   </div>
+  <!-- CARD SLIDER -->
+  <div class="mx-8 text-right">
+    <!-- CARD -->
+    <div class="rounded-lg bg-blue">
+      <img src="../../images/visa_logo.png" alt="" class="w-16 h-4">
+      <!-- DECORATION CARD -->
+      <div class="">
+        <div class="w-8 h-full rounded-full bg-yellow"></div> 
+        <div class="w-8 h-full rounded-full bg-yellow"></div>
+        <div class="w-8 h-full rounded-full bg-yellow"></div>
+        <div class="w-8 h-full rounded-full bg-yellow"></div>
+
+      </div>
+      <span class="text-white">************8052</span>
+      <span class="block text-2xl font-bold text-white">$12,480.40</span>
+    </div>
+  </div>
+  
 </template>
 <script>
 export default {};
